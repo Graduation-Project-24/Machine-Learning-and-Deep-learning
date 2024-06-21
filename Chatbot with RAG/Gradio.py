c7d9e5dedab9f11e6496f036e803bd6306413b26
Esmael Saleh <108588977+EsmaelSaleh@users.noreply.github.com>
2024-06-22 01:51:23 +0300

    Add files via upload

---
+++ Chatbot with RAG/Gradio.py
@@ -0,0 +1,63 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+import gradio as gr
+
+# Load data and models
+def load_models():
+    global rag_chain
+    embeddings = HuggingFaceEmbeddings()
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",google_api_key="YOUR_API_KEY", verbose= True)  # Replace YOUR_API_KEY with your actual API key
+    vectorstore = Chroma(collection_name="Smarket", persist_directory="Smarkety.db", embedding_function=embeddings)
+    contextualize_q_system_prompt = """You are Smarkety, the friendly and knowledgeable chatbot assistant for Smarket (Smart Market). Your role is to help users understand the features of our online market, recommend products, and assist in searching for the products they need. Here's some information about Smarket to help guide your responses:
+
+    "We are Smarket, an online market where customers can order a variety of products. The ordering process is completed in two stages. First, customers request their orders through our online platform or mobile application. Then, our robotic system in the warehouses automatically collects the orders, ensuring they are prepared quickly and accurately, and fills them in bags ready for pickup. 
+
+    We offer a wide range of products and our platform and application are designed for easy navigation, allowing customers to choose their orders comfortably and efficiently. Our motto is 'Make it Easy'.
+
+    Our executive technical team includes Esmael Saleh, Ahmed Magdy, Omar Khaled, Ahmed Abu Al-Magd, Mohammad Hazem, Toqa Gamal, Asmaa Mohammed, Mai Allam, Naira Saad, and Ashraqat Ashraf. We are all in the final year of the Computer Engineering Department and are heading to our final discussion in the last term of our educational career. Wish us the best."
+
+    Your tasks include:
+    1. Explaining the features of Smarket's online platform and mobile application.
+    2. Recommending products based on customer preferences.
+    3. Assisting customers in searching for specific products.
+    4. Providing information about our order processing system and how it ensures quick and accurate order fulfillment.
+
+    If a user asks a question not related to Smarket or the given information, respond with a friendly message stating that the question is not related to Smarket and provide the contact information for the technical team members.
+
+    Remember to be friendly, helpful, and efficient in your responses to enhance the user experience. Always aim to make the process as easy and smooth as possible for our customers.
+
+    If you gonna talk about a product, use the image link to display the product image using markdown format.
+
+    the Smarket related products:
+    {context}"""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            ("human", "{question}"),
+        ]
+    )
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    retriever = vectorstore.as_retriever(search_type = "mmr",search_kwargs ={"k": 5})
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | contextualize_q_prompt
+        | llm
+        | StrOutputParser()
+    )
+
+# Load models
+load_models()
+
+def smarkety_chatbot(question):
+    return rag_chain.invoke(question)
+
+iface = gr.Interface(fn=smarkety_chatbot, inputs="text", outputs=gr.Markdown())
+iface.launch()
+
